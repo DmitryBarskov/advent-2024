bf673aa2bcf04f9c24f037df9a8637b6b7124760
Dmitry Barskov <dmitry.barskov64@gmail.com>
2024-12-04 12:44:46 +0100

    Solve day 04**

--- day04.ex
+++ day04.ex
@@ -1,42 +1,41 @@
 defmodule Day04 do
-  @spec count_word(grid :: {{String}}, word :: charlist()) :: integer
-  def count_word(grid, word) do
-    rows = (0..(tuple_size(grid) - 1))
+  @spec count_x_mas(grid :: {{String}}) :: integer
+  def count_x_mas(grid) do
+    rows = (1..(tuple_size(grid) - 2))
     Enum.reduce(
       rows,
       0,
       fn row, acc ->
-        cols = (0..(tuple_size(elem(grid, row)) - 1))
+        cols = (1..(tuple_size(elem(grid, row)) - 2))
         Enum.reduce(
           cols,
           acc,
           fn col, cnt ->
-            cnt + count_word_in_position(grid, row, col, word)
+            if has_x_mas_in_position(grid, row, col) do
+              cnt + 1
+            else
+              cnt
+            end
           end
         )
       end
     )
   end
 
-  def count_word_in_position(grid, row, col, word) do
-    [{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}]
-    |> Enum.count(fn {dy, dx} -> has_word_in_direction(grid, row, col, word, dy, dx) end)
-  end
+  def get(grid, row, col), do: grid |> elem(row) |> elem(col)
 
-  def has_word_in_direction(_, _, _, [], _, _), do: true
-  def has_word_in_direction(_, row, col, _, _, _) when row < 0 or col < 0, do: false
-  def has_word_in_direction(grid, row, _, _, _, _) when row >= tuple_size(grid), do: false
-  def has_word_in_direction(grid, row, col, _, _, _) when col >= tuple_size(elem(grid, row)), do: false
-  def has_word_in_direction(grid, row, col, [letter | rest], dy, dx) do
-    (grid |> elem(row) |> elem(col)) == letter and \
-      has_word_in_direction(grid, row + dy, col + dx, rest, dy, dx)
+  def has_x_mas_in_position(grid, row, col) do
+    letters = [{"M", "S"}, {"S", "M"}]
+    get(grid, row, col) == "A" and \
+      Enum.member?(letters, {get(grid, row - 1, col - 1), get(grid, row + 1, col + 1)}) and \
+      Enum.member?(letters, {get(grid, row - 1, col + 1), get(grid, row + 1, col - 1)})
   end
 
   def main() do
     Lib.read_lines()
     |> Enum.map(&Lib.string_to_tuple/1)
     |> List.to_tuple
-    |> count_word(String.split("XMAS", "", trim: true))
+    |> count_x_mas()
     |> IO.inspect(charlists: :as_lists, limit: :infinity)
   end
 end
