d98a9b0785d38fec1d219ff70c6aad387ab51cf5
Dmitry Barskov <dmitry.barskov64@gmail.com>
2024-12-08 17:36:42 +0100

    Solve day 8 **

--- day08.ex
+++ day08.ex
@@ -11,12 +11,8 @@ defmodule Day08 do
     city
     |> find_antennas
     |> Enum.group_by(fn {freq, _, _} -> freq end)
-    |> find_all_atninodes
-    |> Enum.filter(
-      fn {y, x} ->
-        0 <= y and y < city_height and 0 <= x and x < city_width
-      end
-    )
+    |> find_all_atninodes({city_height, city_width})
+    |> Enum.filter(&in_city(&1, {city_height, city_width}))
     |> length
     |> IO.inspect()
   end
@@ -40,33 +36,38 @@ defmodule Day08 do
     )
   end
 
-  def find_all_atninodes(antennas_by_frequency) do
+  def find_all_atninodes(antennas_by_frequency, city) do
     antennas_by_frequency
     |> Enum.reduce(
       MapSet.new(),
-      fn {_freq, antennas}, set -> find_antinodes(antennas, set) end
+      fn {_freq, antennas}, set -> find_antinodes(antennas, set, city) end
     )
   end
 
-  def find_antinodes(antennas, result \\ MapSet.new())
-  def find_antinodes([], result), do: result
-  def find_antinodes([{_freq, y1, x1} | rest], result) do
+  def find_antinodes([], result, _), do: result
+  def find_antinodes([{_freq, y1, x1} | rest], result, city) do
     antinodes = Enum.reduce(
       rest, 
       result,
       fn {_freq, y2, x2}, antinodes ->
-        [an1, an2] = antinodes_for_two_antennas({y1, x1}, {y2, x2})
-        antinodes |> MapSet.put(an1) |> MapSet.put(an2)
+        ans = antinodes_for_two_antennas({y1, x1}, {y2, x2}, city)
+        Enum.reduce(ans, antinodes, fn an, acc -> MapSet.put(acc, an) end)
       end
     )
-    find_antinodes(rest, antinodes)
+    find_antinodes(rest, antinodes, city)
   end
 
-  def antinodes_for_two_antennas({y1, x1}, {y2, x2}) do
+  def antinodes_for_two_antennas({y1, x1}, {y2, x2}, city) do
     {dy, dx} = {y1 - y2, x1 - x2}
-    [
-      {y1 + dy, x1 + dx},
-      {y2 - dy, x2 - dx}
-    ]
+    right =
+      Stream.unfold({y1, x1}, fn {y, x} -> {{y, x}, {y + dy, x + dx}} end)
+      |> Enum.take_while(&in_city(&1, city))
+    left =
+      Stream.unfold({y1, x1}, fn {y, x} -> {{y, x}, {y - dy, x - dx}} end)
+      |> Enum.take_while(&in_city(&1, city))
+    left ++ right
   end
+
+  def in_city({y, x}, {city_height, city_width}),
+    do: 0 <= y and y < city_height and 0 <= x and x < city_width
 end
